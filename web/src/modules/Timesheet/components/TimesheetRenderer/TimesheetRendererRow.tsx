ff1f3be9bebe26cc29c92505a93a08884e90c938
DanielSerio <dserio.d@gmail.com>
2025-06-17 08:35:38 -0400

    added column for note

--- web/src/modules/Timesheet/components/TimesheetRenderer/TimesheetRendererRow.tsx
+++ web/src/modules/Timesheet/components/TimesheetRenderer/TimesheetRendererRow.tsx
@@ -1,5 +1,6 @@
 import type { TimesheetViewMode } from "#modules/Timesheet/hooks/useTimesheetViewMode";
 import type { TimesheetLineRecord } from "#types/entity/timesheet.types";
+import { Text } from "@mantine/core";
 
 export interface TimesheetRendererRowProps {
   viewMode: TimesheetViewMode;
@@ -10,6 +11,9 @@ export function TimesheetRendererRow({ row }: TimesheetRendererRowProps) {
   return (
     <li>
       <span>{row.id}</span>
+      <Text title={row.note ?? undefined} lineClamp={1}>
+        {row.note}
+      </Text>
       <span>{row.startTime}</span>
       <span>{row.endTime}</span>
       <span>00:00</span>
